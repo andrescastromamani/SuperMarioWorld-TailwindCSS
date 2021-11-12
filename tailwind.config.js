2917d22458bd5e6834a2f3c3e9058930a983db34
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-11-11 20:44:37 -0400

    We add the Hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: theme => ({
+                'wave-pattern':'url(../img/wave-white.png)'
+            })
+        },
     },
     variants: {
         extend: {},
